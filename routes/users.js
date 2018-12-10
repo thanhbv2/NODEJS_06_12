86559eda0e06765699cabb1b980d0835ef8c792f
thanhbv2 <thanhbv007@gmail.com>
2018-12-10 11:17:28 +0700

    config init

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
-let data = require('./mockdata')
+let data = require('./mockdata');
+
+const mockup = [
+  { text: 'learn node js', completed: true, completedAt: Date.now() },
+  { text: 'learn Vode js', completed: true, completedAt: Date.now() },
+  { text: 'learn Rode js', completed: true, completedAt: Date.now() },
+  { text: 'learn webpack', completed: true, completedAt: Date.now() },
+  { text: 'learn babel core', completed: true, completedAt: Date.now() }
+]
 
 
 /* GET users listing. */
@@ -10,14 +18,13 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/', (req, res, next) => {
-  const { name, location } = req.body;
   // console.log('===============>', data.users.push({ id: 3, name, location }));
-  data.users.push({ id: 3, name, location });
-  console.log('===============>', __dirname);
+  mockup.push(req.body);
   // fs.writeFileSync(`${__dirname}/test.json`, JSON.stringify(data));
   // let test = fs.readFileSync(`${__dirname}/test.json`);
   // console.log('===============>', JSON.parse(test));
-  res.json(data.users);
+  console.log('===============>', mockup);
+  res.json(mockup);
 })
 
 
